268fd0cd6a1fa69713aff9f204356dda70915494
AlirezA <bojbaj@gmail.com>
2019-06-14 02:00:07 +0430

    use services to fetch shows data

---
+++ Kookweb.SubtitleTranslator.Api/Controllers/ShowsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kookweb.SubtitleTranslator.Domain.Models;
+using Kookweb.SubtitleTranslator.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kookweb.SubtitleTranslator.Api.Controllers {
+
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class ShowsController : ControllerBase {
+        private readonly ITVShowService _tvShowService;
+        public ShowsController (ITVShowService tvShowService) {
+            _tvShowService = tvShowService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TVShow>>> Get () {
+
+            IEnumerable<TVShow> shows = await _tvShowService.ListAsync ();
+            return Ok (shows);
+        }
+    }
+}
---
+++ Kookweb.SubtitleTranslator.Api/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kookweb.SubtitleTranslator.Domain.Persistence.Contexts;
+using Kookweb.SubtitleTranslator.Domain.Repositories;
+using Kookweb.SubtitleTranslator.Domain.Services;
+using Kookweb.SubtitleTranslator.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Kookweb.SubtitleTranslator.Api {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<AppDbContext> (options => {
+                options.UseInMemoryDatabase ("subtitle-translator-api-in-memory");
+            });
+
+            services.AddScoped<ITVShowRepository, TVShowRepository> ();
+            services.AddScoped<ITVShowService, TVShowService> ();
+        }
+
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            }
+
+            app.UseMvc ();
+        }
+    }
+}
