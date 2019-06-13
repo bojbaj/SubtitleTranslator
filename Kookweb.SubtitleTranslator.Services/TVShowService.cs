906246f311f23cbbe948c61e6039e9b3a1e04e72
AlirezA <bojbaj@gmail.com>
2019-06-14 01:59:43 +0430

    Services implemented

---
+++ Kookweb.SubtitleTranslator.Services/TVShowService.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Kookweb.SubtitleTranslator.Domain.Models;
+using Kookweb.SubtitleTranslator.Domain.Repositories;
+using Kookweb.SubtitleTranslator.Domain.Services;
+
+namespace Kookweb.SubtitleTranslator.Services {
+    public class TVShowService : ITVShowService {
+        private readonly ITVShowRepository _tvShowRepository;
+
+        public TVShowService (ITVShowRepository tvShowRepository) {
+            this._tvShowRepository = tvShowRepository;
+        }
+        public Task<IEnumerable<TVShow>> ListAsync () {
+            return _tvShowRepository.ListAsync ();
+        }
+    }
+}
